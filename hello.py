26a61ceb17ea673f0af0ac57830cd3c514667c3a
Vic <2603067@sit.singaporetech.edu.sg>
2026-09-04 14:51:39 +0800

    Dynamic Profile

--- hello.py
+++ hello.py
@@ -1,17 +1,11 @@
+username = input("Enter Username:")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
 print("======================")
-print("Welcome here")
-print("My first post!")
-print("======================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
 
-username = "cool_creator"
-bio = "Fun Blogger"
-followers = 100
-
-followers += 50
-print("Day 1:", followers)
-
-followers += 20
-print("Day 2:", followers)
-
-followers -= 10
-print("Day 3:", followers)
+    
