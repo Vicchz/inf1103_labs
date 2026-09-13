df26872374e9836df837ae03860d2f6cb66b33d7
Vic <2603067@sit.singaporetech.edu.sg>
2026-09-13 14:17:11 +0800

    Initialize inventory

---
+++ Week_2lab/auditor.py
@@ -0,0 +1,19 @@
+'''Initialize Inventory to 0
+
+run a loop asking enter stock quantity untill type quit
+
+accept stock as integer
+
+handle invalid input etc ten
+
+reject nagtive number
+
+keep inventory updated 
+
+if inventory > 500, alert and break loop
+
+when type quit, print total unit processed and numbe failed rejected entries'''
+
+inventory = 0
+
+
